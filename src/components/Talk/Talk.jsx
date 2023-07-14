a4c2e0f0fd86561421e357617da24c5979ed0809
GersenX3 <galdl1438@gmail.com>
2023-07-13 18:39:51 -0600

    Using the animitation in the end of the page

--- src/components/Talk/Talk.jsx
+++ src/components/Talk/Talk.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { FadeInOnScroll } from "../FadeInOnScroll/FadeInOnScroll.jsx"
 import "../Talk/Talk.css"
 
 function Talk() {
   return (
+    <FadeInOnScroll>
     <div className='talk'>
-        <span>SEND ME A TEXT - FEEL FREE TO REACH OUT - GET IN TOUCH - INITIATE A CHAT -</span>
+        <span className='talk-span'>SEND ME A TEXT - FEEL FREE TO REACH OUT - GET IN TOUCH - INITIATE A CHAT - SEND ME A TEXT </span>
     </div>
+    </FadeInOnScroll>
   )
 }
 export {Talk}
